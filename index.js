9a7a76709d973357832c2990a5855b0baed61548
MReid28 <79878110+MReid28@users.noreply.github.com>
2021-04-07 21:52:37 -0400

    Task 3

--- index.js
+++ index.js
@@ -28,11 +28,14 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  The difference is that counter1 is local and counter2 is global.
   
   2. Which of the two uses a closure? How can you tell?
+  The first one is a closure because it has a function inside of a function.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
 */
 
 // counter1 code
@@ -62,10 +65,10 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(number){
+    return Math.floor(Math.random()*3++)
 }
-
+console.log(inning)
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
